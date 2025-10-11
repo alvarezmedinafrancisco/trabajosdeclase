f29776d9542f0d6e81b31419319e53b1d50fbbd9
alvarezmedinafrancisco <23308060610104@cetis61.edu.mx>
2025-10-10 19:39:30 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,40 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return """"<h1>bienvenido a la calculadora de python </h1>"
-"<h2>1.- para sumar escribe en el navegador 127.0.0.1:5000/suma/v1/v2</h2>"
-"<h2>2.- para restar escribe en el navegador 127.0.0.1:5000/resta/v1/v2</h1>"
-"<h2>3.- para multiplicar escribe en el navegador 127.0.0.1:5000/multiplica/v1/v2</h1>"
-"<h2>4.- para dividir escribe en el navegador 127.0.0.1:5000/divide/v1/v2</h1>"
-"<h2>5.- para saber el valor maximo y minimo escribe en el navegador 127.0.0.1:5000/maxmin/v1/v2</h1>" """
-    
-    
-@app.route('/devide/<int:v1>/<int:v2>')
-def devide(v1,v2):
-        if v2!=0:
-            resultado =v1/v2
-            return f"<h3>{resultado}</h3>"
+def inicio():
+    return render_template('base.html')
 
-@app.route('/suma/<int:v1>/<int:v2>')
-def suma(v1,v2):
-        resultado=v1+v2
-        return f"<h3>{resultado}</h3>"
+@app.route('/index1')
+def animales():
+    return render_template('index1.html')
 
-@app.route('/resta/<int:v1>/<int:v2>')
-def resta(v1,v2):
-        resultado = v1 - v2
-        return f"<h3>{resultado}</h3>"
-    
-@app.route('/multiplica/<int:v1>/<int:v2>')
-def multiplica(v1, v2):  
-        resultado = v1 * v2
-        return f"<h3>{resultado}</h3>"
-    
-@app.route('/maxmin/<int:v1>/<int:v2>')
-def maxmin(v1, v2):
-        maximo = max(v1, v2)
-        minimo = min(v1, v2)
-        return f"<h1>El máximo entre {v1} y {v2} es {maximo}</h1><h1>El mínimo entre {v1} y {v2} es {minimo}</h1>"
+@app.route('/index2')
+def vehiculos():
+    return render_template('index2.html')
+
+@app.route('/index3')
+def maravillas():
+    return render_template('index3.html')
+
+@app.route('/acerca')
+def acercade():
+    return render_template('acerca.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)

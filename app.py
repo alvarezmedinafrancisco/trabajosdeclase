06f1e8dbe69dd44b3df12b4e5c6eeb0db093b859
alvarezmedinafrancisco <23308060610104@cetis61.edu.mx>
2025-10-27 08:54:51 -0500

    commit

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request , redirtect , url_for , sesion
+from flask import Flask, render_template, request , url_for, redirect, session
 app = Flask(__name__)
 
+usuario= [{
+    "usuario": "admin",
+    "contra": "admin123"
+}]
 @app.route('/')
 def inicio():
     return render_template('base.html')
@@ -21,13 +25,13 @@ def maravillas():
 def acercade():
     return render_template('acerca.html')
 
-@app.route('/secion', methods=["GET", "POST"])
-def secion():
+@app.route('/validalogin', methods=["GET", "POST"])
+def validalogin():
     error = None
     if request.method == "POST":
         contra = request.form.get("contra")
         newcontra = request.form.get("newcontra")
-        boton = request.form.get("boton")
+        
         
         if not contra or not newcontra:
             error = "Este campo es requerido."
@@ -38,7 +42,23 @@ def secion():
             if boton == "boton":
                 return render_template('base.html', error=error)
             
-    return render_template('secion.html', error=error)
+    return render_template('registro.html', error=error)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    error = None
+    if request.method == "POST":
+        usuario = request.form.get("usuario")
+        contra = request.form.get("contra")
+        
+        if not usuario or not contra:
+            error = "Este campo es requerido."
+        else:
+            error = "Registro exitoso."
+            return render_template('base.html', error=error)
+            
+    return render_template('login.html', error=error)
 
 
 if __name__ == '__main__':
